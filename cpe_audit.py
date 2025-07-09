e0c6a77cd16ed86ba08757a927bc5a4237b4336a
Sahil <sahilratnaparkhi1@gmail.com>
2025-07-09 12:51:45 +0530

    changed the sql to exclude mod columns

--- cpe_audit.py
+++ cpe_audit.py
@@ -15,8 +15,7 @@ def get_cpe_audit(id):
 
     print('Connection Successfull')
 
-    query = 'SELECT aud.*, CONCAT(first_name, \' \', last_name) AS `User`, revtstmp FROM catissue_coll_prot_event_aud aud JOIN os_revisions os ON aud.rev = os.rev JOIN catissue_user cu ON os.user_id = cu.identifier WHERE aud.identifier = %s;'
-
+    query = 'SELECT aud.IDENTIFIER, aud.REV, aud.REVTYPE, aud.COLLECTION_POINT_LABEL, aud.STUDY_CALENDAR_EVENT_POINT, aud.ACTIVITY_STATUS, aud.CLINICAL_DIAGNOSIS, aud.CLINICAL_STATUS, aud.DEFAULT_SITE_ID, aud.CODE, aud.VISIT_NAME_PRINT_COPIES, aud.EVENT_POINT_UNIT, aud.CLINICAL_STATUS_ID, aud.CLINICAL_DIAGNOSIS_ID, CONCAT(cu.first_name, \' \', cu.last_name) AS `User`, os.revtstmp FROM catissue_coll_prot_event_aud aud JOIN os_revisions os ON aud.rev = os.rev JOIN catissue_user cu ON os.user_id = cu.identifier WHERE aud.identifier = %s;'
     cursor.execute(query,(id,))
 
     rows = cursor.fetchall()
--- sr_audit.py
+++ sr_audit.py
@@ -15,7 +15,7 @@ def get_cpe_audit(id):
 
     print('Connection Successfull')
 
-    query = 'SELECT aud.*, CONCAT(first_name, \' \', last_name) AS `User`, revtstmp FROM catissue_cp_req_specimen_aud aud JOIN os_revisions os ON aud.rev = os.rev JOIN catissue_user cu ON os.user_id = cu.identifier WHERE aud.identifier = %s;'
+    query = "SELECT aud.IDENTIFIER, aud.REV, aud.REVTYPE, aud.SPEC_REQ_LABEL, aud.LINEAGE, aud.SPECIMEN_CLASS, aud.SPECIMEN_TYPE, aud.TISSUE_SITE, aud.TISSUE_SIDE, aud.PATHOLOGICAL_STATUS, aud.STORAGE_TYPE, aud.INITIAL_QUANTITY, aud.CONCENTRATION, aud.COLLECTION_PROCEDURE, aud.COLLECTION_CONTAINER, aud.LABELFORMAT, aud.ACTIVITY_STATUS, aud.COLLECTOR_ID, aud.RECEIVER_ID, aud.SORT_ORDER, aud.CODE, aud.POOLED_SPMN_REQ_ID, aud.LABEL_PRINT_COPIES, aud.ANATOMIC_SITE_ID, aud.LATERALITY_ID, aud.COLLECTION_PROCEDURE_ID, aud.COLLECTION_CONTAINER_ID, aud.SPECIMEN_CLASS_ID, aud.SPECIMEN_TYPE_ID, aud.PATHOLOGICAL_STATUS_ID, aud.DEFAULT_CUSTOM_FIELD_VALUES, aud.PRE_BARCODED_TUBE, CONCAT(cu.first_name, ' ', cu.last_name) AS `User`, os.revtstmp FROM catissue_cp_req_specimen_aud aud JOIN os_revisions os ON aud.rev = os.rev JOIN catissue_user cu ON os.user_id = cu.identifier WHERE aud.identifier = %s;"
 
     cursor.execute(query,(id,))
 
